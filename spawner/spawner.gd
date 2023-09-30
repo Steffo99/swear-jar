ff2137367f7f7f3e5fd183186389182796d35ee7
Matteo Balugani <supermatte.mb@gmail.com>
2023-09-30 20:16:14 +0200

    made basic spawner

---
+++ spawner/spawner.gd
@@ -0,0 +1,10 @@
+extends Node2D
+class_name Spawner
+@export var scene: PackedScene
+@export var target_parent: NodePath
+
+func spawn():
+	var scene_instant= scene.instantiate()
+	scene_instant.position=Vector2.ZERO
+	add_child(scene_instant)
+	print(scene_instant.global_position)
