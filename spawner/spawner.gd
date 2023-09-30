0f51e2e8d6b119ac3b136c228dc33d04b69befb6
Stefano Pigozzi <me@steffo.eu>
2023-10-01 00:05:15 +0200

    Prevent spawn if coins would overlap

--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -4,7 +4,7 @@ class_name Spawner
 @export var target_parent: NodePath
 
 func spawn():
-	if len($Area2D.get_overlapping_bodies()) > 0:
+	if len($Area.get_overlapping_bodies()) > 0:
 		return
 	var scene_instant = scene.instantiate()
 	scene_instant.position=Vector2.ZERO
