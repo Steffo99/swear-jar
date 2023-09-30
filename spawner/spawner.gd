f844cd02d22abdd8d1ce89eb86c75d7c2c1a5f34
Stefano Pigozzi <me@steffo.eu>
2023-10-01 01:33:54 +0200

    Configure spawners

--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -1,12 +1,18 @@
 extends Node2D
 class_name Spawner
+
+
 @export var scene: PackedScene
-@export var target_parent: NodePath
+
 
 func spawn():
-	if len($Area.get_overlapping_bodies()) > 0:
-		return
-	var scene_instant = scene.instantiate()
-	scene_instant.position=Vector2.ZERO
-	add_child(scene_instant)
+	var overlapping_bodies = $Area.get_overlapping_bodies()
+	for overlapping_body in overlapping_bodies:
+		if overlapping_body.collision_layer && 0b100:
+			print("[Spawner] Not spawning, overlapping with: ", overlapping_body)
+			return
 	
+	var scene_instant = scene.instantiate()
+	scene_instant.position = Vector2.ZERO
+	print("[Spawner] Created: ", scene_instant)
+	add_child(scene_instant)
