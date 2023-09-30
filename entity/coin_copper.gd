5f5337c91f12c0d02d9b2c09b88ef3846a17b566
Stefano Pigozzi <me@steffo.eu>
2023-09-30 22:53:38 +0200

    "Fix" physics

---
+++ entity/coin_copper.gd
@@ -0,0 +1,5 @@
+extends RigidBody2D
+class CoinCopper
+
+
+func _integrate_forces(state: PhysicsDirectBodyState2D):
--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -4,7 +4,9 @@ class_name Spawner
 @export var target_parent: NodePath
 
 func spawn():
+	if len($Area2D.get_overlapping_bodies()) > 0:
+		return
+
 	var scene_instant= scene.instantiate()
 	scene_instant.position=Vector2.ZERO
 	add_child(scene_instant)
-	print(scene_instant.global_position)
