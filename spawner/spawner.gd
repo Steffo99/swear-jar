265da5fe506b342b725f01aafc11ae2d1a334b34
Stefano Pigozzi <me@steffo.eu>
2023-10-01 01:58:46 +0200

    Cache $Area reference in Spawner

--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -7,6 +7,8 @@ class_name Spawner
 var buffer: int = 0
 @export var buffer_cap: int
 
+@onready var area: Area2D = $Area
+
 
 func spawn():
 	buffer = clampi(buffer + 1, 0, buffer_cap)
@@ -14,14 +16,11 @@ func spawn():
 
 func _physics_process(_delta):
 	if buffer > 0:
-		var overlapping_bodies = $Area.get_overlapping_bodies()
+		var overlapping_bodies = area.get_overlapping_bodies()
 		for overlapping_body in overlapping_bodies:
 			if overlapping_body.collision_layer && 0b100:
-				print("[Spawner] Not spawning, overlapping with: ", overlapping_body)
-				return
-		
+				return		
 		var scene_instant = scene.instantiate()
 		scene_instant.position = Vector2.ZERO
-		print("[Spawner] Spawned ", buffer, "/", buffer_cap, ": ", scene_instant)
 		buffer -= 1
 		add_child(scene_instant)
