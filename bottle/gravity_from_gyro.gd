40089db03ae7071cc58c1a33b7b2adf365edb959
Stefano Pigozzi <me@steffo.eu>
2023-10-01 01:37:25 +0200

    Default to down gravity if accelerometer is not supported

--- bottle/gravity_from_gyro.gd
+++ bottle/gravity_from_gyro.gd
@@ -4,6 +4,8 @@ class_name GravityFromGyro
 
 func _physics_process(_delta):
 	var accel_3d = Input.get_accelerometer()
+	if accel_3d == Vector3.ZERO:  # If accelerometer is not supported
+		accel_3d = Vector3.DOWN * 9.8
+	
 	var accel_2d = Vector2(accel_3d.x, -accel_3d.y) / 9.8
 	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, accel_2d)
-
