8382919fd11152a25f37a9f9bf687a8e771abed5
Matteo Balugani <supermatte.mb@gmail.com>
2023-10-01 23:27:05 +0200

    added animation for copper converter

--- convertors/copper_to_silver.gd
+++ convertors/copper_to_silver.gd
@@ -2,11 +2,15 @@ extends StaticBody2D
 
 @export var scene: PackedScene
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
 func _on_collector_collected(body):
 	body.queue_free()
 
-
 func _on_collector_goal():
-	print("silver")
+	$AnimatedSprite2D.play()
+	$Timer.start()
+
+func _on_timer_timeout():
+	$AnimatedSprite2D.stop()
 	$Spawner.spawn()
-	
