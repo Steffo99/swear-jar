c9d93048b54be9ea431a83967955e1066964abfe
Matteo Balugani <supermatte.mb@gmail.com>
2023-10-02 00:18:57 +0200

    converter kinda works

--- convertors/copper_to_silver.gd
+++ convertors/copper_to_silver.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+var coda : int = 0
+
 @export var scene: PackedScene
 
 @onready var _animated_sprite = $AnimatedSprite2D
@@ -8,11 +10,16 @@ func _on_collector_collected(body):
 	body.queue_free()
 
 func _on_collector_goal():
-	$AnimatedSprite2D.play()
-	$converter_working.play()
-	$Timer.start()
+	coda+=1
+
+func _process(delta):
+	if coda>=1 and $Timer.is_stopped():
+		$AnimatedSprite2D.play()
+		$converter_working.play()
+		$Timer.start()
 
 func _on_timer_timeout():
+	coda-=1
 	$AnimatedSprite2D.stop()
 	$converter_working.stop()
 	$New_item.play()
