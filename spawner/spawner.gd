a10335c0b9c58c450886ea25a4461fa107c64594
Stefano Pigozzi <me@steffo.eu>
2023-10-01 02:18:08 +0200

    Spawn coins at random positions along the shape extents

--- spawner/spawner.gd
+++ spawner/spawner.gd
@@ -8,21 +8,34 @@ var buffer: int = 0
 @export var buffer_cap: int
 
 @onready var area: Area2D = $Area
+@onready var shape: CollisionShape2D = $Area/Shape
+
+@onready var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 
 
 func spawn():
 	buffer += 1
 	if buffer > buffer_cap:
+		print("Hit buffer!")
 		buffer = buffer_cap
+
+
+func _select_spawn_position() -> Vector2:
+	var variance = shape.shape.size.x / 2
+	var x = rng.randf_range(-variance, +variance)
+	return Vector2(x, 0)
+
+func _do_spawn():
+	var overlapping_bodies = area.get_overlapping_bodies()
+	for overlapping_body in overlapping_bodies:
+		if overlapping_body.collision_layer && 0b100:
+			return
+	var scene_instant = scene.instantiate()
+	scene_instant.position = _select_spawn_location()
+	add_child(scene_instant)
+	buffer -= 1
 
 
 func _physics_process(_delta):
 	if buffer > 0:
-		var overlapping_bodies = area.get_overlapping_bodies()
-		for overlapping_body in overlapping_bodies:
-			if overlapping_body.collision_layer && 0b100:
-				return		
-		var scene_instant = scene.instantiate()
-		scene_instant.position = Vector2.ZERO
-		buffer -= 1
-		add_child(scene_instant)
+		_do_spawn()
