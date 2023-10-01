a3826b9801215aee210335f8f10b4d998a8742b7
Stefano Pigozzi <me@steffo.eu>
2023-10-01 03:20:47 +0200

    Document collector entity

--- collector/collectible.gd
+++ collector/collectible.gd
@@ -1,23 +1,18 @@
 extends Node
 class_name Collectible
+## A marker for collectible entities.
+##
+## Used by [Collector]s to determine which entities to pick up.
 
-enum CollectibleType {
-	UNSET,
-	COIN_COPPER,
-	COIN_SILVER,
-	COIN_GOLD,
-	GEM,
-	DIAMOND,
-	COAL,
-	CROWN,
-	SUPERCROWN,
-}
 
-@export var type: CollectibleType
+## The type of collectible entity the parent entity represents.
+@export var type: StringName
 
+## Emitted when this entity has been collected by a collector.
 signal collected
 
-
+## Mark this entity as collected.
+##
+## You'll probably want to connect this to an AudioSource2D, which will disable the node and play a sound, and when the sound is over a new signal will queue_free it.
 func collect():
 	emit_signal("collected")
-	get_parent().queue_free()
--- collector/collector.gd
+++ collector/collector.gd
@@ -1,13 +1,29 @@
 extends Area2D
 class_name Collector
+## Area that will pick up [Collectible]s with a given name, keeping track of the amount collected.
 
 
+## The current amount of collected entities.
 var collected_count: int = 0
 
-@export var collecting_types: Array[Collectible.CollectibleType]
+## The types of [Collectible]s to pick up.
+##
+## The strings will match only if exactly the same.
+@export var collecting_types: Array[StringName]
+
+## The collision mask to check colliding body against.
 @export_flags_2d_physics var collecting_collision_mask: int
 
+## The goal amount of entities to collect.
+##
+## When [collected_count] reaches it, it will be reset to zero, and the "goal" signal will be emitted.
+@export var collecting_amount: int
+
+## The collector has picked up an object.
 signal collected(what: PhysicsBody2D)
+
+## The collector has received its collection goal and is about to reset.
+signal goal
 
 
 func _on_body_entered(body: Node2D):
@@ -18,3 +34,6 @@ func _on_body_entered(body: Node2D):
 				collected_count += 1
 				collectible.collect()
 				emit_signal("collected", body)
+				if collected_count >= collecting_amount:
+					emit_signal("goal")
+					collected_count = 0
