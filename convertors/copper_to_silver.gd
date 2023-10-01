a0762fcdc25f35f783e492ace8a58e00604d0ebb
Matteo Balugani <supermatte.mb@gmail.com>
2023-10-01 23:44:40 +0200

    more audio for converter

--- convertors/copper_to_silver.gd
+++ convertors/copper_to_silver.gd
@@ -9,8 +9,11 @@ func _on_collector_collected(body):
 
 func _on_collector_goal():
 	$AnimatedSprite2D.play()
+	$converter_working.play()
 	$Timer.start()
 
 func _on_timer_timeout():
 	$AnimatedSprite2D.stop()
+	$converter_working.stop()
+	$New_item.play()
 	$Spawner.spawn()
