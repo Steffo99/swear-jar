bc43de64d9e9a5a5e872afd60cd976fae11c7478
Stefano Pigozzi <me@steffo.eu>
2023-10-02 18:42:10 +0200

    Fix gem value

--- entity/gem/RandomValue.gd
+++ entity/gem/RandomValue.gd
@@ -1,4 +1,9 @@
 extends Node
 
+
+@export var min_value: int
+@export var max_value: int
+
+
 func _ready():
-	get_parent().value=Randomizer.rng.randi_range(0,360)
+	get_parent().value = Randomizer.rng.randi_range(min_value, max_value)
