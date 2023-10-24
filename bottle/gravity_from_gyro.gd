cdb7ae2816f7b4206f95f5a9a30bc823eb47e2bd
Stefano Pigozzi <me@steffo.eu>
2023-10-24 20:44:13 +0200

    Add a slight default gravity towards the bottom

--- bottle/gravity_from_gyro.gd
+++ bottle/gravity_from_gyro.gd
@@ -9,6 +9,9 @@ func _physics_process(_delta):
 	var accel_3d = Input.get_accelerometer()
 	if accel_3d == Vector3.ZERO:  # If accelerometer is not supported
 		accel_3d = (Vector3.UP * Input.get_action_strength("gravity_up") + Vector3.DOWN * Input.get_action_strength("gravity_down") + Vector3.LEFT * Input.get_action_strength("gravity_left") + Vector3.RIGHT * Input.get_action_strength("gravity_right")) * 9.8
+	if accel_3d == Vector3.ZERO:  # If nothing is being pressed
+		accel_3d = (Vector3.DOWN * 0.05) * 9.8
+
 	
 	var accel_2d = Vector2(accel_3d.x, -accel_3d.y) / 9.8
 	PhysicsServer2D.area_set_param(get_viewport().find_world_2d().space, PhysicsServer2D.AREA_PARAM_GRAVITY_VECTOR, accel_2d)
